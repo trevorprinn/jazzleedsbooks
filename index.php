cbb12c2df66837635faedf00ae7a004e05e52cb8
Trevor Prinn <trev@babbacom.com>
2018-06-24 18:45:29 +0100

    Cosmetic improvements.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include('header.php');
 
 <p>Info will go here.</p>
 
-<p>Go to the <a href="booklist.php">Book List</a>, select the books you are  interested in, then
+<p>Go to the <a href="booklist.php">Book List</a>, select the books you are interested in, then
 go to the Interested list, and send us a message so we can bring the books along to a gig.</p>
 
 <div class="panel panel-primary">
