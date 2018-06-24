2461276019c0ffafd9d6e72b48f7467b0f0355f9
Trevor Prinn <trev@babbacom.com>
2018-06-24 18:30:51 +0100

    Added bio.

--- index.php
+++ index.php
@@ -9,5 +9,36 @@ include('header.php');
 <p>Go to the <a href="booklist.php">Book List</a>, select the books you are  interested in, then
 go to the Interested list, and send us a message so we can bring the books along to a gig.</p>
 
+<div class="panel panel-primary">
+	<div class="panel-heading">
+		<h3 class="panel-title">
+			<a class="accordian-toggle collapsed" data-toggle="collapse" data-target="#bio">Bill Sheldrake (28/09/1934 - 20/11/2011)</a>
+		</h3>
+	</div>
+	<div id="bio" class="panel-collapse collapse">
+		<div class="panel-body">
+			<p>Bill had 2 loves in his life - his family and listening to music - in particular Jazz.</p>
+			<p>An only child, he loved to solve puzzles and read, often having several books on the go at once.</p>
+			<p>He met his wife Audrey whilst they were both in the army. They spent their first 12 years together
+			in Kenya, and several places in Germany before settling down in Kent with their 4 children.</p>
+			<p>He was a humble, hard working man, who did everything he could to provide a good home for his
+			wife and family. He lost Audrey in 1997, and at this point, also bringing up 2 grandchildren,
+			he found great solace listening to music.</p>
+			<p>He spent many years campaigning on behalf of Mental Health users and carers, having spent
+			many years as a carer for 2 of his children who have complex Mental Health needs, and during
+			this time he built up an extensive library of books, especially books on Jazz. He devoured as
+			much information as he could, read as many biographies/autobiographies as he could and collected
+			many pictorial history music books.</p>
+			<p>He felt very lucky to meet Maggie during his later years,
+			and they spent 6 happy years together, sharing a love of music and travelling - he was absolutely
+			thrilled to see George Melly several times and also to go to Ronnie Scott's and celebrate his
+			75th Birthday - before he passed away in 2011.</p>
+			<p>It means a great deal to me, his daughter, as it would to Bill, that other people will now
+			be able to share in the enjoyment that he found in his collection of Jazz books.</p>
+			<p><strong><em>Susan Gush</em></strong></p>
+			<img src="images/Bill Sheldrake.jpg" alt="Bill Sheldrake"/>
+		</div>
+	</div>
+</div>
 
 <?php include('footer.php'); ?>
