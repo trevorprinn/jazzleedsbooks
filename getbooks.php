cc97d461f9a01765b7a657e9f2ca13008868e0ae
Trevor Prinn <trev@babbacom.com>
2020-05-03 16:25:42 +0100

    Pick up book list from librarything using the server as the proxy, rather than reading from a downloaded file.

--- getbooks.php
+++ getbooks.php
@@ -3,6 +3,6 @@ header("Content-Type: application/json;charset=utf-8");
 
 session_start();
 
-echo file_get_contents('./books.json', true);
+echo file_get_contents('http://www.librarything.com/api_getdata.php?userid=jazzleeds&key=2837672999&booksort=title&max=350&showTags=1&responseType=json&showCollections=1', true);
 
 ?>
